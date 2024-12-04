27ba4aa55d5c0b2b8338af8a05ac6af04f9e7c0d
Matthieu <matthieu.jeanson@gmail.com>
2024-12-03 22:52:05 -0800

    Day 4 part 2

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     read_file(&mut data);
 
     let mut result1: u32 = 0;
+    let mut result2: u32 = 0;
 
     for y in 0..data.len() {
         for x in 0..data[y].len() {
@@ -65,10 +66,19 @@ fn main() {
                     0
                 };
             }
+
+            if y >= 1 && y < data.len() - 1 && x >= 1 && x < data[y].len() - 1 {
+                result2 += if search_for_x_mas(&data, (x, y)) {
+                    1
+                } else {
+                    0
+                }
+            }
         }
     }
 
     println!("Found {} XMAS", result1);
+    println!("Found {} X-MAS", result2);
 }
 
 fn read_file(data: &mut Vec<Vec<char>>) {
@@ -89,6 +99,21 @@ fn search_for_xmas(data: &Vec<Vec<char>>, starting: (usize, usize), direction: (
         && data[(y as i32 + dy) as usize][(x as i32 + dx) as usize] == 'M'
         && data[(y as i32 + 2 * dy) as usize][(x as i32 + 2 * dx) as usize] == 'A'
         && data[(y as i32 + 3 * dy) as usize][(x as i32 + 3 * dx) as usize] == 'S'
+    {
+        true
+    } else {
+        false
+    }
+}
+
+fn search_for_x_mas(data: &Vec<Vec<char>>, starting: (usize, usize)) -> bool {
+    let (x, y) = starting;
+
+    if data[y][x] == 'A'
+        && ((data[y - 1][x - 1] == 'M' && data[y + 1][x + 1] == 'S')
+            || (data[y - 1][x - 1] == 'S' && data[y + 1][x + 1] == 'M'))
+        && ((data[y - 1][x + 1] == 'M' && data[y + 1][x - 1] == 'S')
+            || (data[y - 1][x + 1] == 'S' && data[y + 1][x - 1] == 'M'))
     {
         true
     } else {
