fc554d95806ed70254bc49502d0dff8fe0841243
Matthieu <matthieu.jeanson@gmail.com>
2024-12-20 23:57:47 -0800

    Day 17 part 2

--- day_17/src/main.rs
+++ day_17/src/main.rs
@@ -47,33 +47,187 @@ fn main() {
     }
     println!("{:?}", current_state.instructions);
 
-    let mut result: Vec<u64> = Vec::new();
-    loop {
-        match current_state.instructions[current_state.pc] {
-            0 => 
-                current_state.reg_a = current_state.reg_a / (1 << get_combo_operand(&current_state)),
-            
-            1 => current_state.reg_b = current_state.reg_b ^ get_literal_operand(&current_state),
-            2 => current_state.reg_b = get_combo_operand(&current_state) % 8,
-            3 => {
-                if current_state.reg_a != 0 {
-                    current_state.pc = get_literal_operand(&current_state) as usize;
-                    continue;
-                }
-            },
-            4 => current_state.reg_b ^= current_state.reg_c,
-            5 => result.push(get_combo_operand(&current_state) % 8),
-            6 => current_state.reg_b = current_state.reg_a / (1 << get_combo_operand(&current_state)),
-            7 => current_state.reg_c = current_state.reg_a / (1 << get_combo_operand(&current_state)),
-            _ => panic!(),
+    let mut _result: Vec<u64> = Vec::new();
+
+    /* after a few tries, we find that all the values of a that reach at least 5 items in the program cycle through an addition of
+         66824
+    256
+    63992
+    1504520
+    256
+    261888
+    256
+    199160
+    66824
+    256
+    63992
+    1966080
+
+    and we can start at 1079733
+
+    then we repeat the process to find another start and increases sequence
+     */
+
+    let mut init_a = 596353314493;
+    let adds = [
+        256,
+        67108608,
+        256,
+        68652367616,
+        256,
+        67108608,
+        256,
+        885904113408,
+        256,
+        2952789760,
+        256,
+        67108608,
+        256,
+        67108608,
+        256,
+        67108608,
+        256,
+        67108608,
+        256,
+        141599702784,
+        256,
+        67108608,
+        256,
+        68652367616,
+        256,
+        67108608,
+        256,
+        1030725041920,
+        256,
+        67108608,
+        256,
+        68652367616,
+        256,
+        67108608,
+        256,
+        637056057088,
+        256,
+        8388352,
+        256,
+        8388352,
+        256,
+        83885824,
+        256,
+        248747392768,
+        256,
+        2952789760,
+        256,
+        67108608,
+        256,
+        67108608,
+        256,
+        67108608,
+        256,
+        67108608,
+        256,
+        22808624896,
+        256,
+        8388352,
+        256,
+        8388352,
+        256,
+        83885824,
+        256,
+        2592079616,
+        256,
+        67108608,
+        256,
+        67108608,
+        256,
+        67108608,
+        256,
+        325388792,
+        1766664,
+        256,
+        8388352,
+        256,
+        8388352,
+        256,
+        100663040,
+        256,
+        184171888384,
+        256,
+        67108608,
+        256,
+        1030725041920,
+    ];
+    let mut adds_index = 0;
+    let mut pc_match;
+
+    'outer: loop {
+        init_a += adds[adds_index];
+        adds_index += 1;
+        if adds_index == adds.len() {
+            adds_index = 0;
         }
-        current_state.pc += 2;
-        if current_state.pc >= current_state.instructions.len() {
-            break;
+        pc_match = 0;
+        current_state.reg_a = init_a;
+        current_state.pc = 0;
+        current_state.reg_b = 0;
+        current_state.reg_c = 0;
+        print!("\r{} ", current_state.reg_a);
+
+        loop {
+            match current_state.instructions[current_state.pc] {
+                0 => {
+                    current_state.reg_a =
+                        current_state.reg_a / (1 << get_combo_operand(&current_state))
+                }
+
+                1 => {
+                    current_state.reg_b = current_state.reg_b ^ get_literal_operand(&current_state)
+                }
+                2 => current_state.reg_b = get_combo_operand(&current_state) % 8,
+                3 => {
+                    if current_state.reg_a != 0 {
+                        current_state.pc = get_literal_operand(&current_state) as usize;
+                        continue;
+                    }
+                }
+                4 => current_state.reg_b ^= current_state.reg_c,
+                5 => {
+                    /* result.push(get_combo_operand(&current_state) % 8) */
+                    let x = (get_combo_operand(&current_state) % 8) as u8;
+                    if current_state.instructions[pc_match] == x {
+                        print!("{}, ", x);
+                        pc_match += 1;
+                    } else {
+                        if pc_match >= 12 {
+                            println!("");
+                        }
+                        continue 'outer;
+                    }
+                }
+                6 => {
+                    current_state.reg_b =
+                        current_state.reg_a / (1 << get_combo_operand(&current_state))
+                }
+                7 => {
+                    current_state.reg_c =
+                        current_state.reg_a / (1 << get_combo_operand(&current_state))
+                }
+                _ => panic!(),
+            }
+            current_state.pc += 2;
+            if current_state.pc >= current_state.instructions.len() {
+                //println!("pc is {}", current_state.pc);
+
+                if pc_match == current_state.instructions.len() {
+                    break 'outer;
+                } else {
+                    break;
+                }
+            }
         }
     }
 
-    println!("{:?}", result);
+    /*println!("{:?}", result);*/
+    println!("{}", init_a);
 }
 
 fn get_literal_operand(state: &Chronospatial) -> u64 {
